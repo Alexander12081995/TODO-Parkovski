69f09a21f23dcdbb330eaf09c1692a8b996340ac
Alexander12081995 <sgen28@gmail.com>
2022-11-24 19:24:24 +0300

    add

--- store.js
+++ store.js
@@ -1,12 +1,16 @@
 export const todos = {
   todoList: [],
+  setTodos(todos) {
+    this.todoList = todos;
+  },
+
   anotherTask(taskName, completed) {
     const newTask = {
       id: Math.random(),
       taskName,
       completed,
     };
-    this.todoList.push(newTask);
+    this.todoList.unshift(newTask);
   },
   deleteTask(taskId) {
     this.todoList = this.todoList.filter((task) => task.id !== taskId);
--- views.js
+++ views.js
@@ -5,6 +5,7 @@ const addTask = document.querySelector(".addTask");
 const form = document.querySelector(".form");
 const buttonThemeLight = document.querySelector(".button_theme__light");
 const buttonThemeDark = document.querySelector(".button_theme__dark");
+const searchInput = document.querySelector(".search_input");
 
 buttonThemeLight.addEventListener("click", () => {
   const themeName = document.body.getAttribute("data-theme");
@@ -22,33 +23,58 @@ buttonThemeDark.addEventListener("click", () => {
   }
 });
 
+const themeFromStorage = localStorage.getItem("theme");
+document.body.setAttribute("data-theme", themeFromStorage);
+
 function renderTodo() {
   ul.innerHTML = null;
+  if (todos.todoList.length === 0) {
+    const text = document.createElement("p");
+    text.classList.add("text_task");
+    text.textContent = "СПИСОК ЗАДАЧ ПУСТ";
+    return ul.append(text);
+  }
   todos.todoList.forEach((todo) => {
     ul.innerHTML += `
         <li class="list_task">
         <div>
-          <p>"${todo.taskName}"</p>
+          <p>${todo.taskName}</p>
               <div class="completed">
-                <input type="checkbox" class="checkbox" data-checkbox-id="${todo.id}">
-                <p class="completed_checkbox">"${todo.completed}"</p>
+                <input type="checkbox" class="checkbox" data-checkbox-id="${
+                  todo.id
+                }" ${todo.checked ? "checked" : ""}>
+                <p class="completed_checkbox">Выполнено</p>
               </div>
         </div>  
         
           
         <div class="group_button">
-        <button type="submit" data-edit-id="${todo.id}" class="task_edit">Изменить</button>
-        <button type="submit" data-delete-id="${todo.id}" class="task_delete">Удалить</button>
+        <button type="submit" data-edit-id=${
+          todo.id
+        } class="task_edit">Изменить</button>
+        <button type="submit" data-delete-id=${
+          todo.id
+        } class="task_delete">Удалить</button>
         </div>
         </li>
         `;
   });
 }
 
+function checkTask() {
+  ul.addEventListener("click", (e) => {
+    const checkId = e.target.getAttribute("data-checkbox-id");
+    let taskId = todos.todoList.find((item) => item.id === +checkId);
+    if (taskId) {
+      taskId.checked = !taskId.checked;
+    }
+    localStorage.setItem("tasks", JSON.stringify(todos.todoList));
+    renderTodo();
+  });
+}
+
 function addTodo() {
-  const task = {
-    completed: false,
-  };
+  const task = {};
   addTask.addEventListener("click", (e) => {
     e.preventDefault();
 
@@ -59,8 +85,10 @@ function addTodo() {
       alert("ВВЕДИТЕ ЗАДАЧУ");
     } else {
       todos.anotherTask(task.taskName, task.completed);
+
       form.reset();
       renderTodo();
+      localStorage.setItem("tasks", JSON.stringify(todos.todoList));
     }
   });
 }
@@ -70,10 +98,29 @@ function deleteTodo() {
     const taskId = e.target.getAttribute("data-delete-id");
     if (taskId) {
       todos.deleteTask(Number(taskId));
+      localStorage.setItem("tasks", JSON.stringify(todos.todoList));
       renderTodo();
     }
   });
 }
 
+function searchTask() {
+  searchInput.addEventListener("input", (e) => {
+    if (e.target.value === "") {
+      todos.todoList = JSON.parse(localStorage.getItem("tasks"));
+    }
+    todos.todoList = JSON.parse(localStorage.getItem("tasks"));
+    todos.todoList = todos.todoList.filter(
+      (todo) => todo.taskName.includes(e.target.value) === true
+    );
+    renderTodo();
+  });
+}
+
+checkTask();
+searchTask();
+let todosFromStorage = JSON.parse(localStorage.getItem("tasks")) || [];
+todos.setTodos(todosFromStorage);
+renderTodo();
 addTodo();
 deleteTodo();
